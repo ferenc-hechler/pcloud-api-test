015cae5dd1fe18f923b4b9d28427da008b87679e
feri <github@fh.anderemails.de>
2021-12-13 00:13:57 +0100

    fix pom.

--- src/main/java/de/hechler/experiments/pcloud/PCloudApiMain.java
+++ src/main/java/de/hechler/experiments/pcloud/PCloudApiMain.java
@@ -1,9 +1,54 @@
 package de.hechler.experiments.pcloud;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.pcloud.sdk.*;
+
+/**
+ * Test using pCloud API - Java SDK
+ * https://github.com/pCloud/pcloud-sdk-java 
+ * 
+ * SDK docu: https://pcloud.github.io/pcloud-sdk-java/
+ * 
+ * API docu: https://docs.pcloud.com/
+ * 
+ * @author feri
+ */
+
 public class PCloudApiMain {
 
-	public static void main(String[] args) {
+	private final static String ENV_FILENAME = ".env";
+
+	private static class Config {
+		private String client_id;
+		private String client_secret;
+	}
+	
+	private static Config readConfig() throws IOException, FileNotFoundException {
+		Properties envProps = new Properties();
+		envProps.load(new FileInputStream(ENV_FILENAME));
+		Config result = new Config();
+		result.client_id = envProps.getProperty("CLIENT_ID");
+		result.client_secret = envProps.getProperty("CLIENT_SECRET");
+		return result;
+	}
+
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		System.out.println(PCloudApiMain.class + " started");
+		
+		Config config = readConfig();
+		
+		String oauthAccessToken = config.client_id+":"+config.client_secret;
+		ApiClient apiClient = PCloudSdk.newClientBuilder()
+                .authenticator(Authenticators.newOAuthAuthenticator(oauthAccessToken))
+                // Other configuration...
+                .create();		
+		Call<RemoteFolder> rootFolderInfo = apiClient.listFolder("/", false);
+		System.out.println(rootFolderInfo);
 	}
 
 }
